38fb84aefadb542c264ce2a02c6c40367f032c6a
FarzanehMG <farzaneh.golian2001@gmail.com>
2023-12-26 16:27:05 +0330

    fix form and columns class letter LetterAttachment

--- CorrespondenceSystem/CorrespondenceSystem.Web/Modules/ServerTypes/LetterAttachmentDB/LetterAttachmentColumns.ts
+++ CorrespondenceSystem/CorrespondenceSystem.Web/Modules/ServerTypes/LetterAttachmentDB/LetterAttachmentColumns.ts
@@ -3,14 +3,8 @@ import { Column } from "@serenity-is/sleekgrid";
 import { LetterAttachmentRow } from "./LetterAttachmentRow";
 
 export interface LetterAttachmentColumns {
-    Id: Column<LetterAttachmentRow>;
-    LetterIdentifier: Column<LetterAttachmentRow>;
     Title: Column<LetterAttachmentRow>;
     AttachmentFile: Column<LetterAttachmentRow>;
-    CreatedDate: Column<LetterAttachmentRow>;
-    CreatorUserName: Column<LetterAttachmentRow>;
-    ModifiedDate: Column<LetterAttachmentRow>;
-    ModifiedUserName: Column<LetterAttachmentRow>;
 }
 
 export class LetterAttachmentColumns extends ColumnsBase<LetterAttachmentRow> {
