f901a7b870a483919a8b80166ce13e1721881238
FarzanehMG <farzaneh.golian2001@gmail.com>
2024-01-03 09:59:57 +0330

    Implement Upload just word file for template and false isDefault fields avalibale record in database

---
+++ CorrespondenceSystem/CorrespondenceSystem.Web/Modules/TemplateDB/DTO/CheckIsDefault.cs
@@ -0,0 +1,8 @@
+namespace CorrespondenceSystem.Modules.TemplateDB.DTO;
+
+public class CheckIsDefault
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public bool IsDefault { get; set; }
+}
--- CorrespondenceSystem/CorrespondenceSystem.Web/Modules/TemplateDB/Template/RequestHandlers/TemplateSaveHandler.cs
+++ CorrespondenceSystem/CorrespondenceSystem.Web/Modules/TemplateDB/Template/RequestHandlers/TemplateSaveHandler.cs
@@ -1,3 +1,4 @@
+using CorrespondenceSystem.Modules.TemplateDB.DTO;
 using Serenity.Services;
 using MyRequest = Serenity.Services.SaveRequest<CorrespondenceSystem.TemplateDB.TemplateRow>;
 using MyResponse = Serenity.Services.SaveResponse;
@@ -18,5 +19,45 @@ public class TemplateSaveHandler : SaveRequestHandler<MyRow, MyRequest, MyRespon
     {
         Row.Id = Guid.NewGuid();
         base.ValidateRequest();
+    }
+
+    protected override void BeforeSave()
+    {
+        if (!Row.TemplateFile.EndsWith(".docx", StringComparison.OrdinalIgnoreCase))
+        {
+            throw new Exception("please upload doc file");
+        }
+
+        //var sql = "SELECT IsDefault FROM Template WHERE IsDefault = 1";
+        //var result = Connection.Query<bool>(sql);
+
+        //if (Row.IsDefault == true)
+        //{
+        //    foreach (var item in result)
+        //    {
+
+        //    }
+        //}
+
+
+        if (Row.IsDefault.Value)
+        {
+            IEnumerable<CheckIsDefault> isDefaultRecords = Connection.Query<CheckIsDefault>(@"SELECT Id, Title, IsDefault FROM Template WHERE IsDefault = 1");
+
+            if (isDefaultRecords.Count() != 0 && Row.Id.Value != isDefaultRecords.FirstOrDefault().Id)
+            {
+                foreach (var item in isDefaultRecords)
+                {
+                    // Update each record in the database to set IsDefault to false
+                    Connection.Execute("UPDATE Template SET IsDefault = 0 WHERE Id = @Id", new { Id = item.Id });
+                }
+
+                // Set the IsDefault field of the current record to true
+                Connection.Execute("UPDATE Template SET IsDefault = 1 WHERE Id = @Id", new { Id = Row.Id });
+            }
+        }
+
+
+        base.BeforeSave();
     }
 }
