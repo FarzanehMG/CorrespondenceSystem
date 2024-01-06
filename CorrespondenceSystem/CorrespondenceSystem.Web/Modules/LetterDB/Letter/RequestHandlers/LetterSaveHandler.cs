90e02aec0a956720488ba49c80df69b75ac3652a
Maryam <maryamentezar44@gmail.com>
2024-01-06 16:21:01 +0330

    saving records to LetterHistory

--- CorrespondenceSystem/CorrespondenceSystem.Web/Modules/LetterDB/Letter/RequestHandlers/LetterSaveHandler.cs
+++ CorrespondenceSystem/CorrespondenceSystem.Web/Modules/LetterDB/Letter/RequestHandlers/LetterSaveHandler.cs
@@ -1,3 +1,4 @@
+using CorrespondenceSystem.LetterHistoryDB;
 using Serenity.Services;
 using MyRequest = Serenity.Services.SaveRequest<CorrespondenceSystem.LetterDB.LetterRow>;
 using MyResponse = Serenity.Services.SaveResponse;
@@ -19,5 +20,60 @@ public class LetterSaveHandler : SaveRequestHandler<MyRow, MyRequest, MyResponse
         //Row.CreatedDate = DateTime.Now;
         Row.Id = Guid.NewGuid();
         base.ValidateRequest();
+    }
+    protected override void AfterSave()
+    {
+
+
+
+        base.AfterSave();
+        short actiontype = 0;
+
+        if (IsCreate)
+        {
+            actiontype = 1;
+        }
+        else if (IsUpdate)
+        {
+            actiontype = 2;
+        }
+        else
+        {
+            actiontype = 3;
+        }
+        var Letterhistory = new LetterHistoryRow { ActionType = actiontype };
+        MapTo(Row, Letterhistory);
+        Connection.Insert<LetterHistoryRow>(Letterhistory);
+
+    }
+    public void MapTo(IRow src, IRow des)
+    {
+
+        foreach (var s in src.GetFields())
+        {
+            foreach (var d in des.GetFields())
+            {
+                if (d.Name == "Id")
+                {
+                    Guid Id = Guid.NewGuid();
+                    des[d.Name] = Id;
+                }
+                else if (d.Name == "LetterId")
+                {
+
+                    des[d.Name] = src["Id"];
+                }
+                else if (s.Name == d.Name && s.Type == d.Type)
+                {
+                    
+                    
+                        des[d.Name] = src[s.Name];
+                    
+
+                }
+            }
+        }
+
+
     }
 }
