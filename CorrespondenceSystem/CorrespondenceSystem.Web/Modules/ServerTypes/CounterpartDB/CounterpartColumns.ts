dc41b29cce1e9c544aa6c893979a3da4a7e6cf92
FarzanehMG <farzaneh.golian2001@gmail.com>
2024-01-07 11:47:55 +0330

    add changes

--- CorrespondenceSystem/CorrespondenceSystem.Web/Modules/ServerTypes/CounterpartDB/CounterpartColumns.ts
+++ CorrespondenceSystem/CorrespondenceSystem.Web/Modules/ServerTypes/CounterpartDB/CounterpartColumns.ts
@@ -3,14 +3,9 @@ import { Column } from "@serenity-is/sleekgrid";
 import { CounterpartRow } from "./CounterpartRow";
 
 export interface CounterpartColumns {
-    Id: Column<CounterpartRow>;
     LetterIdentifier: Column<CounterpartRow>;
-    ReciverName: Column<CounterpartRow>;
     CopyType: Column<CounterpartRow>;
     CreatedDate: Column<CounterpartRow>;
-    CreatorUserName: Column<CounterpartRow>;
-    ModifiedDate: Column<CounterpartRow>;
-    ModifiedUserName: Column<CounterpartRow>;
 }
 
 export class CounterpartColumns extends ColumnsBase<CounterpartRow> {
--- CorrespondenceSystem/CorrespondenceSystem.Web/Modules/ServerTypes/RelatedLettersDB/RelatedLettersColumns.ts
+++ CorrespondenceSystem/CorrespondenceSystem.Web/Modules/ServerTypes/RelatedLettersDB/RelatedLettersColumns.ts
@@ -3,14 +3,9 @@ import { Column } from "@serenity-is/sleekgrid";
 import { RelatedLettersRow } from "./RelatedLettersRow";
 
 export interface RelatedLettersColumns {
-    Id: Column<RelatedLettersRow>;
     LetterIdentifier: Column<RelatedLettersRow>;
-    RelatedLetterId: Column<RelatedLettersRow>;
     RelationType: Column<RelatedLettersRow>;
     CreatedDate: Column<RelatedLettersRow>;
-    CreatorUserName: Column<RelatedLettersRow>;
-    ModifiedDate: Column<RelatedLettersRow>;
-    ModifiedUserName: Column<RelatedLettersRow>;
 }
 
 export class RelatedLettersColumns extends ColumnsBase<RelatedLettersRow> {
