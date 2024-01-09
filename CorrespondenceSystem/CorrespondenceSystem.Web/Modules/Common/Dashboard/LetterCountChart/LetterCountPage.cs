0f815a89f9dd1109c736aaa84b5ddf836d968785
FarzanehMG <farzaneh.golian2001@gmail.com>
2024-01-09 09:26:59 +0330

    convert chart date to persian

--- CorrespondenceSystem/CorrespondenceSystem.Web/Modules/Common/Dashboard/LetterCountChart/LetterCountPage.cs
+++ CorrespondenceSystem/CorrespondenceSystem.Web/Modules/Common/Dashboard/LetterCountChart/LetterCountPage.cs
@@ -13,28 +13,50 @@ public class LetterCountPage : ViewComponent
 
         var Connection = HttpContext.RequestServices.GetRequiredService<ISqlConnections>().NewByKey("CorrespondenceSystem");
 
+        //var IncomingLetters = Connection.Query<LetterCount>(@"
+        //        SELECT YEAR(CreatedDate) AS year,
+        //               MONTH(CreatedDate) AS month,
+        //               COUNT(letterType) AS letterCount
+        //        FROM Letter
+        //        WHERE letterType = 1
+        //        GROUP BY YEAR(CreatedDate), MONTH(CreatedDate);");
+
+        //var OutgoingLetters = Connection.Query<LetterCount>(@"
+        //        SELECT YEAR(CreatedDate) AS year,
+        //               MONTH(CreatedDate) AS month,
+        //               COUNT(letterType) AS letterCount
+        //        FROM Letter
+        //        WHERE letterType = 0
+        //        GROUP BY YEAR(CreatedDate), MONTH(CreatedDate);");
+
         var IncomingLetters = Connection.Query<LetterCount>(@"
-                SELECT YEAR(CreatedDate) AS year,
-                       MONTH(CreatedDate) AS month,
-                       COUNT(letterType) AS letterCount
-                FROM Letter
-                WHERE letterType = 1
-                GROUP BY YEAR(CreatedDate), MONTH(CreatedDate);");
+                    SELECT 
+                    FORMAT(CreatedDate, 'yyyy', 'fa-IR') AS PersianYear,
+                    FORMAT(CreatedDate, 'MM', 'fa-IR') AS PersianMonth,
+                    SUM(CASE WHEN letterType = 1 THEN 1 ELSE 0 END) AS letterCount
+                    FROM Letter
+                    GROUP BY 
+                    FORMAT(CreatedDate, 'yyyy', 'fa-IR'),
+                    FORMAT(CreatedDate, 'MM', 'fa-IR');");
 
         var OutgoingLetters = Connection.Query<LetterCount>(@"
-                SELECT YEAR(CreatedDate) AS year,
-                       MONTH(CreatedDate) AS month,
-                       COUNT(letterType) AS letterCount
-                FROM Letter
-                WHERE letterType = 0
-                GROUP BY YEAR(CreatedDate), MONTH(CreatedDate);");
+                    SELECT 
+                    FORMAT(CreatedDate, 'yyyy', 'fa-IR') AS PersianYear,
+                    FORMAT(CreatedDate, 'MM', 'fa-IR') AS PersianMonth,
+                    SUM(CASE WHEN letterType = 0 THEN 1 ELSE 0 END) AS LetterCount
+                    FROM Letter
+                    GROUP BY 
+                    FORMAT(CreatedDate, 'yyyy', 'fa-IR'),
+                    FORMAT(CreatedDate, 'MM', 'fa-IR');");
 
-        model.CountIncomingLetterNov = IncomingLetters.FirstOrDefault(x => x.month == 11)?.letterCount ?? 0;
-        model.CountIncomingLetterDec = IncomingLetters.FirstOrDefault(x => x.month == 12)?.letterCount ?? 0;
+        model.CountIncomingLetterAban = IncomingLetters.FirstOrDefault(x => x.PersianMonth == 8)?.letterCount ?? 0;
+        model.CountIncomingLetterDey = IncomingLetters.FirstOrDefault(x => x.PersianMonth == 10)?.letterCount ?? 0;
+        model.CountIncomingLetterBahman = IncomingLetters.FirstOrDefault(x => x.PersianMonth == 11)?.letterCount ?? 0;
         // Repeat the above pattern for each month...
 
-        model.CountOutgoingLetterJan = OutgoingLetters.FirstOrDefault(x => x.month == 1)?.letterCount ?? 0;
-        model.CountOutgoingLetterDec = OutgoingLetters.FirstOrDefault(x => x.month == 12)?.letterCount ?? 0;
+        model.CountOutgoingLetterAban = OutgoingLetters.FirstOrDefault(x => x.PersianMonth == 8)?.letterCount ?? 0;
+        model.CountOutgoingLetterDey = OutgoingLetters.FirstOrDefault(x => x.PersianMonth == 10)?.letterCount ?? 0;
+        model.CountOutgoingLetterBahman = OutgoingLetters.FirstOrDefault(x => x.PersianMonth == 11)?.letterCount ?? 0;
         // Repeat the above pattern for each month...
 
         return View(MVC.Views.Common.Dashboard.LetterCountChart.LetterCountIndex, model);
@@ -42,8 +64,8 @@ public class LetterCountPage : ViewComponent
 
     private class LetterCount
     {
-        public int year { get; set; }
-        public int month { get; set; }
+        public int PersianYear { get; set; }
+        public int PersianMonth { get; set; }
         public int letterCount { get; set; }
     }
 }
--- CorrespondenceSystem/CorrespondenceSystem.Web/Modules/Common/Dashboard/LetterCountChart/LetterCountPageModel.cs
+++ CorrespondenceSystem/CorrespondenceSystem.Web/Modules/Common/Dashboard/LetterCountChart/LetterCountPageModel.cs
@@ -2,31 +2,31 @@ namespace CorrespondenceSystem.Modules.Common.Dashboard.LetterCountChart;
 
 public class LetterCountPageModel
 {
-    public int CountIncomingLetterJan { get; set; }
-    public int CountIncomingLetterFeb { get; set; }
-    public int CountIncomingLetterMarch { get; set; }
-    public int CountIncomingLetterApril { get; set; }
-    public int CountIncomingLetterMay { get; set; }
-    public int CountIncomingLetterJune { get; set; }
-    public int CountIncomingLetterJuly { get; set; }
-    public int CountIncomingLetterAugust { get; set; }
-    public int CountIncomingLetterJOct { get; set; }
-    public int CountIncomingLetterSep { get; set; }
-    public int CountIncomingLetterNov { get; set; }
-    public int CountIncomingLetterDec { get; set; }
+    public int CountIncomingLetterFarvardin { get; set; }
+    public int CountIncomingLetterOrdibehesh { get; set; }
+    public int CountIncomingLetterKhordad { get; set; }
+    public int CountIncomingLetterTir { get; set; }
+    public int CountIncomingLetterMordad { get; set; }
+    public int CountIncomingLetterShahrivar { get; set; }
+    public int CountIncomingLetterMehr { get; set; }
+    public int CountIncomingLetterAban { get; set; }
+    public int CountIncomingLetterAzar { get; set; }
+    public int CountIncomingLetterDey { get; set; }
+    public int CountIncomingLetterBahman { get; set; }
+    public int CountIncomingLetterEsfand { get; set; }
 
 
 
-    public int CountOutgoingLetterJan { get; set; }
-    public int CountOutgoingLetterFeb { get; set; }
-    public int CountOutgoingLetterMarch { get; set; }
-    public int CountOutgoingLetterApril { get; set; }
-    public int CountOutgoingLetterMay { get; set; }
-    public int CountOutgoingLetterJune { get; set; }
-    public int CountOutgoingLetterJuly { get; set; }
-    public int CountOutgoingLetterAugust { get; set; }
-    public int CountOutgoingLetterOct { get; set; }
-    public int CountOutgoingLetterSep { get; set; }
-    public int CountOutgoingLetterNov { get; set; }
-    public int CountOutgoingLetterDec { get; set; }
+    public int CountOutgoingLetterFarvardin { get; set; }
+    public int CountOutgoingLetterOrdibehesh { get; set; }
+    public int CountOutgoingLetterKhordad { get; set; }
+    public int CountOutgoingLetterTir { get; set; }
+    public int CountOutgoingLetterMordad { get; set; }
+    public int CountOutgoingLetterShahrivar { get; set; }
+    public int CountOutgoingLetterMehr { get; set; }
+    public int CountOutgoingLetterAban { get; set; }
+    public int CountOutgoingLetterAzar { get; set; }
+    public int CountOutgoingLetterDey { get; set; }
+    public int CountOutgoingLetterBahman { get; set; }
+    public int CountOutgoingLetterEsfand { get; set; }
 }
